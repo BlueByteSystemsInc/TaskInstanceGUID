1fa69a6a95d151aa841172c98b75f9f41acb4fee
Alex Egorchatov <alex.egorchatov@bluebyte.biz>
2021-07-12 12:24:24 -0700

    Changed it to use Interops only.

--- TaskInstanceGUID/TaskInstanceGUIDMain.cs
+++ TaskInstanceGUID/TaskInstanceGUIDMain.cs
@@ -1,33 +1,46 @@
-﻿using BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework;
-using BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework.Attributes;
-using EPDM.Interop.epdm;
+﻿using EPDM.Interop.epdm;
 using System.Runtime.InteropServices;
 
 namespace TaskInstanceGUID
 {
-    [Name("Instance GUID Testing")]
-    [CompanyName("Blue Byte Systems")]
-    [Description("Testing task instance GUID")]
-    [AddInVersion(false, 0)]
-    [RequiredVersion(10, 0)]
-    [IsTask(true)]
-    [TaskFlags((int)EdmTaskFlag.EdmTask_SupportsChangeState + (int)EdmTaskFlag.EdmTask_SupportsDetails + (int)EdmTaskFlag.EdmTask_SupportsInitExec + (int)EdmTaskFlag.EdmTask_SupportsScheduling)]
     [ComVisible(true)]
     [Guid("2800F580-B258-48B8-930C-406D60884AFD")]
-    public class TaskInstanceGUIDMain : AddInBase
+    public class TaskInstanceGUIDMain : IEdmAddIn5
     {
         #region Public Methods
 
-        public override void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
+        public void GetAddInInfo(ref EdmAddInInfo poInfo, IEdmVault5 poVault, IEdmCmdMgr5 poCmdMgr)
         {
-            base.OnCmd(ref poCmd, ref ppoData);
+            poInfo.mbsAddInName = "Instance GUID Testing";
+            poInfo.mbsCompany = "Blue Byte Systems";
+            poInfo.mbsDescription = "Testing task instance GUID";
+            poInfo.mlAddInVersion = 0;
+            poInfo.mlRequiredVersionMinor = 0;
+            poInfo.mlRequiredVersionMajor = 10;
 
+            poCmdMgr.AddHook(EdmCmdType.EdmCmd_TaskLaunch);
+            poCmdMgr.AddHook(EdmCmdType.EdmCmd_TaskRun);
+            poCmdMgr.AddHook(EdmCmdType.EdmCmd_TaskSetup);
+            poCmdMgr.AddHook(EdmCmdType.EdmCmd_TaskSetupButton);
+            poCmdMgr.AddHook(EdmCmdType.EdmCmd_TaskDetails);
+        }
+
+        public void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
+        {
             AttachDebugger();
-
             switch (poCmd.meCmdType)
             {
                 case EdmCmdType.EdmCmd_TaskSetup:
-                    AddContextMenu("Check task GUID...", "It displays current task GUID");
+                    var taskProperties = (poCmd.mpoExtra as IEdmTaskProperties);
+                    taskProperties.TaskFlags = (int)EdmTaskFlag.EdmTask_SupportsChangeState + (int)EdmTaskFlag.EdmTask_SupportsDetails + (int)EdmTaskFlag.EdmTask_SupportsInitExec + (int)EdmTaskFlag.EdmTask_SupportsScheduling;
+
+                    var cmds = new EdmTaskMenuCmd[1] { new EdmTaskMenuCmd() };
+                    cmds[0].mbsMenuString = "Check task GUID...";
+                    cmds[0].mbsStatusBarHelp = "Checks the task instance GUID";
+                    cmds[0].mlCmdID = 1;
+                    cmds[0].mlEdmMenuFlags = (int)EdmMenuFlags.EdmMenu_Nothing;
+                    taskProperties.SetMenuCmds(cmds);
+
                     break;
 
                 case EdmCmdType.EdmCmd_TaskSetupButton:
@@ -40,9 +53,8 @@ namespace TaskInstanceGUID
                     break;
 
                 case EdmCmdType.EdmCmd_TaskLaunch:
-                    var guid1 = Instance.InstanceGUID;
                     var guid2 = (poCmd.mpoExtra as IEdmTaskInstance).InstanceGUID;
-                    Vault.MsgBox(0, $"Instance.InstanceGUID:\n{guid1}\n\n(poCmd.mpoExtra as IEdmTaskInstance).InstanceGUID:\n{guid2}");
+                    (poCmd.mpoVault as IEdmVault5).MsgBox(0, $"Instance GUID:\n{guid2}");
                     break;
 
                 case EdmCmdType.EdmCmd_TaskLaunchButton:
@@ -50,6 +62,17 @@ namespace TaskInstanceGUID
 
                 default:
                     break;
+            }
+        }
+
+        public void AttachDebugger()
+        {
+            System.Diagnostics.Process process = System.Diagnostics.Process.GetCurrentProcess();
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Launch();
             }
         }
 
