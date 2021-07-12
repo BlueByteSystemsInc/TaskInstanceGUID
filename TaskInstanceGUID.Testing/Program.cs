b0e6fcb16a44e74d919e70a791d654b59dac994d
Alex Egorchatov <alex.egorchatov@bluebyte.biz>
2021-07-12 11:17:52 -0700

    Add project files.

---
+++ TaskInstanceGUID.Testing/Program.cs
@@ -0,0 +1,85 @@
+﻿using BlueByte.SOLIDWORKS.PDMProfessional.UnitTesting;
+using BlueByte.SOLIDWORKS.PDMProfessional.UnitTesting.Attributes;
+using BlueByte.SOLIDWORKS.PDMProfessional.UnitTesting.Mockers;
+using EPDM.Interop.epdm;
+using System;
+
+namespace TaskInstanceGUID.Testing
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            var test = new TestClass();
+            test.Run();
+            Console.ReadLine();
+        }
+
+        #endregion
+    }
+
+    [TestVault(Name = "BlueByte")]
+    public class TestClass : TestableAddInBase<TaskInstanceGUIDMain>
+    {
+        #region Private Fields
+
+        private string File = @"API\Excel file.xlsx";
+        private IEdmTaskInstance taskInstance;
+        private IEdmTaskProperties taskProperties;
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Startup()
+        {
+            base.Startup();
+            taskInstance = PDMMockFactory.MockEdmTaskInstance(Vault, "GUID Test Task");
+            taskProperties = PDMMockFactory.MockTaskProperties(Vault, "GUID Test Task");
+        }
+
+        [PDMTestMethod()]
+        public void TestTaskSetup()
+        {
+            var poCmd = MockEdmCmd.EdmCmd_TaskSetup.Mock(taskProperties);
+            var poData = MockEdmCmdData.EdmCmd_TaskSetup.Mock();
+            EdmCmdData[] edmCmd = new EdmCmdData[] { poData };
+            AddInObject.OnCmd(ref poCmd, ref edmCmd);
+        }
+
+        [PDMTestMethod()]
+        public void TestTaskSetupButton()
+        {
+            var poCmd = MockEdmCmd.EdmCmd_TaskSetupButton.Mock(taskProperties, "");
+            var poData = MockEdmCmdData.EdmCmd_TaskSetupButton.Mock();
+            EdmCmdData[] edmCmd = new EdmCmdData[] { poData };
+            AddInObject.OnCmd(ref poCmd, ref edmCmd);
+        }
+
+        [PDMTestMethod()]
+        public void TestTaskLaunch()
+        {
+            var object1 = PDMFactory.UseThisVault(Vault).ThenGetFileByRelativePath(File).As<IEdmFile5>();
+            var poCmd = MockEdmCmd.EdmCmd_TaskLaunch.Mock(taskInstance);
+            var poData = MockEdmCmdData.EdmCmd_TaskLaunch.Mock(object1);
+            EdmCmdData[] edmCmd = new EdmCmdData[] { poData };
+            AddInObject.OnCmd(ref poCmd, ref edmCmd);
+        }
+
+        [PDMTestMethod()]
+        public void TestTaskRun()
+        {
+            var object1 = PDMFactory.UseThisVault(Vault).ThenGetFileByRelativePath(File).As<IEdmFile5>();
+            var poCmd = MockEdmCmd.EdmCmd_TaskRun.Mock(taskInstance);
+            var poData = MockEdmCmdData.EdmCmd_TaskRun.Mock(object1);
+            EdmCmdData[] edmCmd = new EdmCmdData[] { poData };
+            AddInObject.OnCmd(ref poCmd, ref edmCmd);
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ TaskInstanceGUID/TaskInstanceGUIDMain.cs
@@ -0,0 +1,58 @@
+﻿using BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework;
+using BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework.Attributes;
+using EPDM.Interop.epdm;
+using System.Runtime.InteropServices;
+
+namespace TaskInstanceGUID
+{
+    [Name("Instance GUID Testing")]
+    [CompanyName("Blue Byte Systems")]
+    [Description("Testing task instance GUID")]
+    [AddInVersion(false, 0)]
+    [RequiredVersion(10, 0)]
+    [IsTask(true)]
+    [TaskFlags((int)EdmTaskFlag.EdmTask_SupportsChangeState + (int)EdmTaskFlag.EdmTask_SupportsDetails + (int)EdmTaskFlag.EdmTask_SupportsInitExec + (int)EdmTaskFlag.EdmTask_SupportsScheduling)]
+    [ComVisible(true)]
+    [Guid("2800F580-B258-48B8-930C-406D60884AFD")]
+    public class TaskInstanceGUIDMain : AddInBase
+    {
+        #region Public Methods
+
+        public override void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
+        {
+            base.OnCmd(ref poCmd, ref ppoData);
+
+            AttachDebugger();
+
+            switch (poCmd.meCmdType)
+            {
+                case EdmCmdType.EdmCmd_TaskSetup:
+                    AddContextMenu("Check task GUID...", "It displays current task GUID");
+                    break;
+
+                case EdmCmdType.EdmCmd_TaskSetupButton:
+                    break;
+
+                case EdmCmdType.EdmCmd_TaskDetails:
+                    break;
+
+                case EdmCmdType.EdmCmd_TaskRun:
+                    break;
+
+                case EdmCmdType.EdmCmd_TaskLaunch:
+                    var guid1 = Instance.InstanceGUID;
+                    var guid2 = (poCmd.mpoExtra as IEdmTaskInstance).InstanceGUID;
+                    Vault.MsgBox(0, $"Instance.InstanceGUID:\n{guid1}\n\n(poCmd.mpoExtra as IEdmTaskInstance).InstanceGUID:\n{guid2}");
+                    break;
+
+                case EdmCmdType.EdmCmd_TaskLaunchButton:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
